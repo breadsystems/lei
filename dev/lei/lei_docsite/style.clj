4e39b00523d3c1ef58bfa195ad74fce770f24161
Coby Tamayo <acobster@gmail.com>
2020-12-12 00:18:48 -0800

    it's the little things

--- dev/lei/lei_docsite/style.clj
+++ dev/lei/lei_docsite/style.clj
@@ -1,6 +1,7 @@
 (ns lei.lei-docsite.style
   (:refer-clojure :exclude [rem])
   (:require
+   [garden.selectors :as s]
    [garden.units :as u :refer [ch percent px em rem]]
    [lei.core :as core]
    [lei.util :as util]))
@@ -11,24 +12,30 @@
 (def screen
   [(core/axioms)
    (core/modular-scale :font-size (em 3.5) :h1 :h2 :h3 :h4 :h5)
-   [:body {:background-color translucent-rose
-           :font-size (px 18)
-           :padding (rem 1)}]
-   [:* {:font-family [:sans-serif]}]
-   [:header {:text-align :center}]
-   [:h1
-    [:&:after {:content "'🌺'"
-               :display :inline-block
-               :margin-left (rem 1)}]]
-   [:h1 :h2 :h3 :h4 :h5 :h6 {:color green}]
-   [:h2 :h3 :h4 :h5 :h6 {:margin-top 0}]
-   [:a {:text-decoration :none
-        :font-weight 700
-        :color green}]
+   ;; General layout stuff
    (core/stack {:recursive? true})
    (core/sidebar {:space (rem 1)
                   :content-min-width (percent 60)
                   :sidebar-width (ch 20)})
+   ;; Spacing
+   (util/line-height)
+   ;; Fonts and colors
+   [:body {:background-color translucent-rose
+           :font-size (px 18)
+           :padding (rem 1)}]
+   [:* {:font-family util/system-sans}]
+   [:h1 :h2 :h3 :h4 :h5 :h6 {:color green
+                             :font-family util/system-serif}]
+   [:h2 :h3 :h4 :h5 :h6 {:margin-top 0}]
+   [:h1
+    [:&:after {:content "'🌺'"
+               :display :inline-block
+               :margin-left (rem 1)}]]
+   [(s/> :header :h2) {:font-style :italic}]
+   [:a {:text-decoration :none
+        :font-weight 700
+        :color green}]
    ;; Misc
+   [:header {:text-align :center}]
    (util/nav>ul)
    (util/pre)])
--- src/lei/util.cljc
+++ src/lei/util.cljc
@@ -1,6 +1,9 @@
 (ns lei.util
+  (:refer-clojure :exclude [*])
   (:require
    [garden.selectors :as s]))
+
+(s/defselector *)
 
 (defn root [m]
   {:pre [(map? m)]}
@@ -12,9 +15,28 @@
   [(s/> :nav :ul) {:list-style :none
                    :padding-left 0}])
 
+(defn line-height
+  "Defaults for body text line-height."
+  []
+  [:p :li :blockquote {:line-height 1.5}])
+
 (defn pre
   "Resets for pre tag: make its contents wrap and fit inside its container."
   []
   [:pre {:overflow-x :auto
          :white-space :pre-wrap
          :word-wrap :break-word}])
+
+(def system-serif
+  ["Georgia" "Times" "Times New Roman" "serif"])
+
+(def system-sans
+  ["-apple-system"
+   "BlinkMacSystemFont"
+   "Segoe UI"
+   "Roboto"
+   "Oxygen-Sans"
+   "Ubuntu"
+   "Cantarell"
+   "Helvetica Neue"
+   "sans-serif"])
