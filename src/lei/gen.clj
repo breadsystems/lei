54caf7b7bae5ba9ec8be4a1335fe90d1aab147ca
Coby Tamayo <acobster@gmail.com>
2020-12-12 12:00:21 -0800

    extract generate fn from gen/-main for reuse

--- src/lei/gen.clj
+++ src/lei/gen.clj
@@ -1,11 +1,28 @@
-(ns lei.gen)
+(ns lei.gen
+  (:require
+   [clojure.string :as str]))
 
-(defn -main [generate & [path]]
-  (let [sym (symbol generate)]
-    (println "Requiring" (namespace sym))
-    (require (symbol (namespace sym)))
-    (let [render (deref (resolve sym))
-          html (render)
-          out (or path "dist/index.html")]
-      (spit out html)
-      (println (format "Output %d bytes to %s" (count html) out)))))
+(defn generate
+  ([handler]
+   (generate handler {}))
+  ([handler opts]
+   (let [sym (symbol handler)
+         {:keys [path]}
+         (merge {:path "dist/index.html"} opts)]
+
+    ;; In a long-running task (i.e. the dev server),
+    ;; we may have previously loaded the handler's ns.
+     (println "Loading" (namespace sym))
+     (require (symbol (namespace sym)) :reload)
+
+     (let [;; Dereference and immediately call our handler.
+           html ((deref (resolve sym)))]
+       (spit path html)
+       (println (format "Output %d bytes to %s" (count html) path))))))
+
+(defn -main [handler & args]
+  {:pre [(even? (count args))]}
+  (let [keywordize (fn [[k v]]
+                     [(keyword (str/replace k #"^:" "")) v])
+        opts (into {} (map keywordize (partition 2 args)))]
+    (generate handler opts)))
